ca183d6803dca5bd91c5198064357fd9deaacdbb
Marcelo Avinte <113268768+Marceloa20@users.noreply.github.com>
2025-06-02 22:02:31 -0300

    Create SELECT names.sql

---
+++ SELECT names.sql
@@ -0,0 +1,24 @@
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+SELECT name FROM world
+  WHERE name LIKE '%Y'
+
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+SELECT name FROM world
+  WHERE name LIKE 'C%ia'
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
